2f0f3c2802caa7b8f39a0e71ddc99476e7dc6f51
Kashiwagi <c0b23038cb@edu.teu.ac.jp>
2024-06-11 18:24:50 +0900

    課題2

--- flying_kokaton.py
+++ flying_kokaton.py
@@ -33,14 +33,18 @@ def main():
         key_1st = pg.key.get_pressed()
 
         if key_1st[pg.K_UP]:
-            kk_rct.move_ip(0,-1)
+            a+=0
+            b+=-1
         if key_1st[pg.K_DOWN]:
-            kk_rct.move_ip(0,1)
+            a += 0
+            b += 1
         if key_1st[pg.K_LEFT]:
-            kk_rct.move_ip(-1,0)
+            a += -1
+            b += 0
         if key_1st[pg.K_RIGHT]:
-            kk_rct.move_ip(2,0)
-        kk_rct.move_ip(-1,0)
+            a += 2
+            b += 0
+        kk_rct.move_ip(-1+a,b)
         screen.blit(kk_img,kk_rct)
         pg.display.update()
         tmr += 1      
